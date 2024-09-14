e912578ac4f8e1a6cc988bf25313a6ad93145a13
Jeet-115 <jeetmistry115@gmail.com>
2024-09-14 23:57:13 +0530

    navbar imported again in all

--- NGPS/src/screens/Aboutus.jsx
+++ NGPS/src/screens/Aboutus.jsx
@@ -1,7 +1,7 @@
 import  { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../components/navbar";
 import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
 
 const Aboutus = () => {
   const ownerSectionRef = useRef(null);
